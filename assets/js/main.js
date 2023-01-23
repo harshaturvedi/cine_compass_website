2809a0f58972c0b63c9a2392627402538c3835a7
harshaturvedi <hchaturvedi320@gmail.com>
2023-01-23 23:00:59 +0530

    added few pages, edited existing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+$(window).on('load', function () {
+    setTimeout(removeLoader, 1000); //wait for page load PLUS two seconds.
+});
+function removeLoader() {
+    $(".preloader").fadeOut(500, function () {
+        // fadeOut complete. Remove the loading div
+        $(".preloader").remove(); //makes page more lightweight 
+    });
+}
+
 function toggleNav() {
     document.getElementById("nav").classList.toggle("show");
 }
